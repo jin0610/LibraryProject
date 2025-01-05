faaf44b5318113ad3e50d467080b8400ff4a44e4
jin610 <sji302777@gmail.com>
2025-01-06 00:50:51 +0900

    update sidebar

--- src/main/frontend/src/components/sample/BookListSidebar.js
+++ src/main/frontend/src/components/sample/BookListSidebar.js
@@ -1,18 +1,28 @@
+import { useState } from "react";
 import "./css/bookList-sidebar.css"
 
 const BookListSidebar = () =>{
 
-    // if($("#bookListSidebar01").hasClass('show') == ture){
+    const [sublist01, setSublist01] = useState(true);
+    const [sublist02, setSublist02] = useState(true);
 
-    // }
+    const handleShow01 = () =>{
+        setSublist01(!sublist01);
+    }
+
+    const handleShow02 = () =>{
+        setSublist02(!sublist02);
+    }
 
     return(
         <aside class="col-xl-3 col-md-12">
             <div class="g-4 card sidebar-card">
                 <div class="card-header justify-content-center">
-                    <a href="#" class="sidebar-sub-title nav-link my-1" role="button" tabindex="0" data-bs-toggle="collapse" data-bs-target="#bookListSidebar01" aria-controls="bookListSidebar01" aria-expanded="false" aria-label="Toggle navigation">
+                    <a href="#" class="sidebar-sub-title nav-link my-1" role="button" tabindex="0" data-bs-toggle="collapse" data-bs-target="#bookListSidebar01" aria-controls="bookListSidebar01" aria-expanded="false" aria-label="Toggle navigation" onClick={handleShow01}>
                         <span class="">분야별</span>
-                        <span><i class="bi bi-caret-down-fill sidebar01-icon"></i></span>
+                        {sublist01 ? (
+                            <span><i class="bi bi-caret-down-fill" ></i></span>
+                        ) : (<span><i class="bi bi-caret-up-fill" ></i></span>)}
                     </a>
                 </div>
                 <div class="sidebar-sub-list collapse show" id="bookListSidebar01">
@@ -45,10 +55,11 @@ const BookListSidebar = () =>{
                     </label>
                 </div>
                 <div class="card-header justify-content-center">
-                    <a href="#" class="nav-link my-1" role="button" tabindex="0" data-bs-toggle="collapse" data-bs-target="#bookListSidebar02" aria-controls="bookListSidebar02" aria-expanded="false" aria-label="Toggle navigation">
+                    <a href="#" class="nav-link my-1" role="button" tabindex="0" data-bs-toggle="collapse" data-bs-target="#bookListSidebar02" aria-controls="bookListSidebar02" aria-expanded="false" aria-label="Toggle navigation" onClick={handleShow02}>
                         <span class="">상품별</span>
-                        <span><i class="bi bi-caret-down-fill"></i></span>
-                        
+                        {sublist02 ? (
+                            <span><i class="bi bi-caret-down-fill" ></i></span>
+                        ) : (<span><i class="bi bi-caret-up-fill" ></i></span>)}
                     </a>
                 </div>
                 <div class="sidebar-sub-list collapse show" id="bookListSidebar02">
