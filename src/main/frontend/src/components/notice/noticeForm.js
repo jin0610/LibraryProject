ae37bc55d1bd59a24b309d2dfb41cad54d917f11
jin610 <sji302777@gmail.com>
2025-05-25 23:50:01 +0900

    Edit Notice Detail Page UI

--- src/main/frontend/src/components/notice/noticeForm.js
+++ src/main/frontend/src/components/notice/noticeForm.js
@@ -14,8 +14,27 @@ const noticeForm = () =>{
                     </div>
                 </div>
             </div>
-            <hr className=""/>
-        
+            <hr className="mb-4"/>
+            <div>
+                <div className="mb-5">
+                    <h4>
+                        제목 1
+                    </h4>
+                </div>
+                <div className="d-flex justify-content-start gap-5">
+                    <p className="pb-1 mb-1">2025-05-25</p>
+                    <p className="pb-1 mb-1">user1</p>
+                    <p className="pb-1 mb-1">조회수 : 25</p>
+                </div>
+            </div>
+            <hr className="mb-4"/>
+            <div className="notice-content">
+                내용입니다~~
+            </div>
+            <hr className="mb-4"/>
+            <div className="d-flex justify-content-end notice-btn">
+                <button className="btn btn-secondary notice-go-to-list-btn">목록</button>
+            </div>
         </div>
     )
 }
